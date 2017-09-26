317d77b4ed198ba6e286071cc714c874f99302be
Marc Alexandre <github@malexandre.fr>
2017-09-26 12:58:14 +0200

    Remove unnecessary expect from Post.list tests

--- src/post/__tests__/list-and-paginate.test.js
+++ src/post/__tests__/list-and-paginate.test.js
@@ -35,7 +35,6 @@ afterEach(async() => {
 
 test('Listing posts should have 10 items', async() => {
     const resp = await Post.list()
-    expect(resp.items).toBeDefined()
     expect(resp.items.length).toBe(10)
 })
 
@@ -46,7 +45,6 @@ test('Listing posts should have more items', async() => {
 
 test('Listing second page should have 5 items', async() => {
     const resp = await Post.list({ offset: 10 })
-    expect(resp.items).toBeDefined()
     expect(resp.items.length).toBe(5)
 })
 
@@ -57,7 +55,6 @@ test('Listing second page should have no more item', async() => {
 
 test('Listing third page should have 0 item', async() => {
     const resp = await Post.list({ offset: 20 })
-    expect(resp.items).toBeDefined()
     expect(resp.items.length).toBe(0)
 })
 
@@ -68,7 +65,6 @@ test('Listing third page should have no more item', async() => {
 
 test('Listing filtered page "Post" should have 5 items', async() => {
     const resp = await Post.list({ filter: 'Post' })
-    expect(resp.items).toBeDefined()
     expect(resp.items.length).toBe(5)
 })
 
@@ -79,7 +75,6 @@ test('Listing filtered page "Post" should have no more item', async() => {
 
 test('Listing 5 posts page should have 5 items', async() => {
     const resp = await Post.list({ count: 5 })
-    expect(resp.items).toBeDefined()
     expect(resp.items.length).toBe(5)
 })
 
@@ -90,8 +85,6 @@ test('Listing 5 posts page should have more items', async() => {
 
 test('Listing 5 posts page order by date should have 5 "Post X" items in order', async() => {
     const resp = await Post.list({ count: 5, orderby: 'date' })
-    expect(resp.items).toBeDefined()
-    expect(resp.items.length).toBe(5)
 
     for (let i = 0; i < 5; ++i) {
         expect(resp.items[i].title).toBe(`Post ${i + 1}`)
@@ -100,8 +93,6 @@ test('Listing 5 posts page order by date should have 5 "Post X" items in order',
 
 test('Listing 5 posts page order by -date should have 5 "Random X" items in order', async() => {
     const resp = await Post.list({ count: 5, orderby: '-date' })
-    expect(resp.items).toBeDefined()
-    expect(resp.items.length).toBe(5)
 
     for (let i = 0; i < 5; ++i) {
         expect(resp.items[i].title).toBe(`Random ${15 - i}`)
