1a36e9fe271a25e2d9196edc2686db1d17d39af7
Marc Alexandre <github@malexandre.fr>
2017-09-26 12:57:50 +0200

    Connect Post.list to the API

---
+++ src/rest/__tests__/api-list.test.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const supertest = require('supertest')
+
+const Api = require('../')
+const Post = require('../../post')
+let app
+
+jest.mock('../../post', () => ({
+    list: jest.fn()
+}))
+
+beforeAll(() => {
+    app = express()
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
+    Api.buildApi(app)
+})
+
+beforeEach(() => {
+    Post.list.mockClear()
+})
+
+test('Calling list with default parameters', async() => {
+    const response = await supertest(app).get('/api/list')
+    expect(response.statusCode).toBe(200)
+    expect(Post.list).toBeCalledWith({ offset: 0, count: 10 })
+})
+
+test('Calling list with one custom orderby', async() => {
+    const response = await supertest(app).get('/api/list?orderby=title')
+    expect(response.statusCode).toBe(200)
+    expect(Post.list).toBeCalledWith({ orderby: 'title', offset: 0, count: 10 })
+})
+
+test('Calling list with multiple custom orderby', async() => {
+    const response = await supertest(app).get('/api/list?orderby=title&orderby=-date')
+    expect(response.statusCode).toBe(200)
+    expect(Post.list).toBeCalledWith({ orderby: ['title', '-date'], offset: 0, count: 10 })
+})
+
+test('Calling list with one custom filter', async() => {
+    const response = await supertest(app).get('/api/list?filter=new%20query')
+    expect(response.statusCode).toBe(200)
+    expect(Post.list).toBeCalledWith({ filter: 'new query', offset: 0, count: 10 })
+})
+
+test('Calling list with multiple custom filter', async() => {
+    const response = await supertest(app).get('/api/list?filter=query&filter=should%20not%20work')
+    expect(response.statusCode).toBe(400)
+    expect(Post.list).not.toBeCalled()
+})
+
+test('Calling list with one custom offset', async() => {
+    const response = await supertest(app).get('/api/list?offset=10')
+    expect(response.statusCode).toBe(200)
+    expect(Post.list).toBeCalledWith({ offset: 10, count: 10 })
+})
+
+test('Calling list with multiple custom offset', async() => {
+    const response = await supertest(app).get('/api/list?offset=10&offset=20')
+    expect(response.statusCode).toBe(400)
+    expect(Post.list).not.toBeCalled()
+})
+
+test('Calling list with one custom count', async() => {
+    const response = await supertest(app).get('/api/list?count=20')
+    expect(response.statusCode).toBe(200)
+    expect(Post.list).toBeCalledWith({ offset: 0, count: 20 })
+})
+
+test('Calling list with multiple custom count', async() => {
+    const response = await supertest(app).get('/api/list?count=10&count=20')
+    expect(response.statusCode).toBe(400)
+    expect(Post.list).not.toBeCalled()
+})
+
+test('Calling list with all arugments', async() => {
+    const response = await supertest(app).get('/api/list?filter=query&offset=10&count=20&orderby=title')
+    expect(response.statusCode).toBe(200)
+    expect(Post.list).toBeCalledWith({ filter: 'query', orderby: 'title', offset: 10, count: 20 })
+})
--- src/rest/index.js
+++ src/rest/index.js
@@ -1,10 +1,39 @@
 'use strict'
 
+const { list } = require('../post')
+
 const fakeHandler = (req, res) => res.sendStatus(200)
 
 const buildApi = (app) => {
     app.get('/api/get', fakeHandler)
-    app.get('/api/list', fakeHandler)
+    app.get('/api/list', async(req, res) => {
+        const count = parseInt(req.query.count)
+        const offset = parseInt(req.query.offset)
+
+        if (typeof req.query.filter !== 'string' && typeof req.query.filter !== 'undefined') {
+            res.status(400).send('Filter should be unique')
+            return
+        }
+
+        if ((isNaN(offset) && typeof req.query.offset !== 'undefined') || Array.isArray(req.query.offset)) {
+            res.status(400).send('Offset should be unique and an integer')
+            return
+        }
+
+        if ((isNaN(count) && typeof req.query.count !== 'undefined') || Array.isArray(req.query.count)) {
+            res.status(400).send('Count should be unique and an integer')
+            return
+        }
+
+        const results = list({
+            filter: req.query.filter,
+            orderby: req.query.orderby,
+            offset: !isNaN(offset) && offset >= 0 ? offset : 0,
+            count: !isNaN(count) && count > 0 ? count : 10
+        })
+
+        res.status(200).json(results)
+    })
 
     app.post('/api/build', fakeHandler)
     app.post('/api/publish', fakeHandler)
