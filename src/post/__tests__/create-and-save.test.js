e53e70b3c53e72db5eb0e463aff41ab66ce35fbf
Marc Alexandre <github@malexandre.fr>
2017-10-02 18:07:31 +0200

    Add save of existing post tests

--- src/post/__tests__/create-and-save.test.js
+++ src/post/__tests__/create-and-save.test.js
@@ -37,3 +37,52 @@ test('Creating a new post should write the file on the file system', async() => 
     const data = fs.readFileSync('content/post/post-test.md', 'utf8')
     expect(data).toBe(sentData)
 })
+
+test('Saving an existing post should return the new post JSON with the tile', async() => {
+    const resp = await Post.save(
+        generatePost(1, 'Post 1', { categories: ['Cat 1', 'Cat 2'], description: 'My description' }, 'Test content')
+    )
+    expect(resp.title).toBe('Post 1')
+})
+
+test('Saving an existing post should return the new post JSON with the categories', async() => {
+    const resp = await Post.save(
+        generatePost(1, 'Post 1', { categories: ['Cat 1', 'Cat 2'], description: 'My description' }, 'Test content')
+    )
+    expect(resp.categories).toEqual(['Cat 1', 'Cat 2'])
+})
+
+test('Saving an existing post should return the new post JSON with the description', async() => {
+    const resp = await Post.save(
+        generatePost(1, 'Post 1', { categories: ['Cat 1', 'Cat 2'], description: 'My description' }, 'Test content')
+    )
+    expect(resp.description).toBe('My description')
+})
+
+test('Saving an existing post should return the new post JSON with the content', async() => {
+    const resp = await Post.save(
+        generatePost(1, 'Post 1', { categories: ['Cat 1', 'Cat 2'], description: 'My description' }, 'Test content')
+    )
+    expect(resp.content).toBe('Test content')
+})
+
+test('Saving an existing post should return the new post JSON with the date', async() => {
+    const resp = await Post.save(
+        generatePost(1, 'Post 1', { categories: ['Cat 1', 'Cat 2'], description: 'My description' }, 'Test content')
+    )
+    expect(resp.date).toEqual(new Date(1 * 1000))
+})
+
+test('Saving an existing post should write the file on the file system', async() => {
+    const oldData = fs.readFileSync('content/post/post-1.md', 'utf8')
+    const sentData = generatePost(
+        1,
+        'Post 1',
+        { categories: ['Cat 1', 'Cat 2'], description: 'My description' },
+        'Test content'
+    )
+    await Post.save(sentData)
+    const data = fs.readFileSync('content/post/post-1.md', 'utf8')
+    expect(data).toBe(sentData)
+    expect(data).not.toBe(oldData)
+})
