8455dea67e63313cee03d1bff225168b52c23565
Marc Alexandre <github@malexandre.fr>
2017-10-02 19:21:27 +0200

    Remove async/await from create & save tests

--- src/post/__tests__/create-and-save.test.js
+++ src/post/__tests__/create-and-save.test.js
@@ -6,74 +6,74 @@ const { generatePost, initMockFs } = require('./setup-common')
 beforeEach(() => initMockFs())
 afterEach(() => mockFs.restore())
 
-test('Creating a new post should return the new post JSON with the tile', async() => {
-    const resp = await Post.save(generatePost(16, 'Post Test'))
+test('Creating a new post should return the new post JSON with the tile', () => {
+    const resp = Post.save(generatePost(16, 'Post Test'))
     expect(resp.title).toBe('Post Test')
 })
 
-test('Creating a new post should return the new post JSON with the categories', async() => {
-    const resp = await Post.save(generatePost(16, 'Post Test', { categories: ['Cat 1', 'Cat 2'] }))
+test('Creating a new post should return the new post JSON with the categories', () => {
+    const resp = Post.save(generatePost(16, 'Post Test', { categories: ['Cat 1', 'Cat 2'] }))
     expect(resp.categories).toEqual(['Cat 1', 'Cat 2'])
 })
 
-test('Creating a new post should return the new post JSON with the description', async() => {
-    const resp = await Post.save(generatePost(16, 'Post Test', { description: 'My description' }))
+test('Creating a new post should return the new post JSON with the description', () => {
+    const resp = Post.save(generatePost(16, 'Post Test', { description: 'My description' }))
     expect(resp.description).toBe('My description')
 })
 
-test('Creating a new post should return the new post JSON with the content', async() => {
-    const resp = await Post.save(generatePost(16, 'Post Test', undefined, 'Test content'))
+test('Creating a new post should return the new post JSON with the content', () => {
+    const resp = Post.save(generatePost(16, 'Post Test', undefined, 'Test content'))
     expect(resp.content).toBe('Test content')
 })
 
-test('Creating a new post should return the new post JSON with the date', async() => {
-    const resp = await Post.save(generatePost(16, 'Post Test', undefined, 'Test content'))
+test('Creating a new post should return the new post JSON with the date', () => {
+    const resp = Post.save(generatePost(16, 'Post Test', undefined, 'Test content'))
     expect(resp.date).toEqual(new Date(16 * 1000))
 })
 
-test('Creating a new post should write the file on the fs', async() => {
+test('Creating a new post should write the file on the fs', () => {
     const sentData = generatePost(16, 'Post Test', undefined, 'Test content')
-    await Post.save(sentData)
+    Post.save(sentData)
     const data = fs.readFileSync('content/post/post-test.md', 'utf8')
     expect(data).toBe(sentData.replace('---', '---\ndraft: true'))
 })
 
-test('Saving an existing post should return the new post JSON with the tile', async() => {
-    const resp = await Post.save(
+test('Saving an existing post should return the new post JSON with the tile', () => {
+    const resp = Post.save(
         generatePost(1, 'Post 1', { categories: ['Cat 1', 'Cat 2'], description: 'My description' }, 'Test content')
     )
     expect(resp.title).toBe('Post 1')
 })
 
-test('Saving an existing post should return the new post JSON with the categories', async() => {
-    const resp = await Post.save(
+test('Saving an existing post should return the new post JSON with the categories', () => {
+    const resp = Post.save(
         generatePost(1, 'Post 1', { categories: ['Cat 1', 'Cat 2'], description: 'My description' }, 'Test content')
     )
     expect(resp.categories).toEqual(['Cat 1', 'Cat 2'])
 })
 
-test('Saving an existing post should return the new post JSON with the description', async() => {
-    const resp = await Post.save(
+test('Saving an existing post should return the new post JSON with the description', () => {
+    const resp = Post.save(
         generatePost(1, 'Post 1', { categories: ['Cat 1', 'Cat 2'], description: 'My description' }, 'Test content')
     )
     expect(resp.description).toBe('My description')
 })
 
-test('Saving an existing post should return the new post JSON with the content', async() => {
-    const resp = await Post.save(
+test('Saving an existing post should return the new post JSON with the content', () => {
+    const resp = Post.save(
         generatePost(1, 'Post 1', { categories: ['Cat 1', 'Cat 2'], description: 'My description' }, 'Test content')
     )
     expect(resp.content).toBe('Test content')
 })
 
-test('Saving an existing post should return the new post JSON with the date', async() => {
-    const resp = await Post.save(
+test('Saving an existing post should return the new post JSON with the date', () => {
+    const resp = Post.save(
         generatePost(1, 'Post 1', { categories: ['Cat 1', 'Cat 2'], description: 'My description' }, 'Test content')
     )
     expect(resp.date).toEqual(new Date(1 * 1000))
 })
 
-test('Saving an existing post should write the file on the fs', async() => {
+test('Saving an existing post should write the file on the fs', () => {
     const oldData = fs.readFileSync('content/post/post-1.md', 'utf8')
     const sentData = generatePost(
         1,
@@ -81,20 +81,20 @@ test('Saving an existing post should write the file on the fs', async() => {
         { categories: ['Cat 1', 'Cat 2'], description: 'My description' },
         'Test content'
     )
-    await Post.save(sentData)
+    Post.save(sentData)
     const data = fs.readFileSync('content/post/post-1.md', 'utf8')
     expect(data).toBe(sentData)
     expect(data).not.toBe(oldData)
 })
 
-test('Saving an existing post without draft status should keep the same draft status on the fs', async() => {
+test('Saving an existing post without draft status should keep the same draft status on the fs', () => {
     const sentNotDraftData = generatePost(
         1,
         'Post 1',
         { categories: ['Cat 1', 'Cat 2'], description: 'My description' },
         'Test content'
     )
-    await Post.save(sentNotDraftData)
+    Post.save(sentNotDraftData)
     const notDraftData = fs.readFileSync('content/post/post-1.md', 'utf8')
     expect(notDraftData).toBe(sentNotDraftData)
 
@@ -104,19 +104,19 @@ test('Saving an existing post without draft status should keep the same draft st
         { categories: ['Cat 1', 'Cat 2'], description: 'My description' },
         'Test content'
     )
-    await Post.save(sentDraftData)
+    Post.save(sentDraftData)
     const data = fs.readFileSync('content/post/random-6.md', 'utf8')
     expect(data).toBe(sentDraftData.replace('---', '---\ndraft: true'))
 })
 
-test('Saving an existing post with a changed draft status should keep the same draft status on the fs', async() => {
+test('Saving an existing post with a changed draft status should keep the same draft status on the fs', () => {
     const sentNotDraftData = generatePost(
         1,
         'Post 1',
         { categories: ['Cat 1', 'Cat 2'], description: 'My description' },
         'Test content'
     )
-    await Post.save(sentNotDraftData.replace('---', '---\ndraft: true'))
+    Post.save(sentNotDraftData.replace('---', '---\ndraft: true'))
     const notDraftData = fs.readFileSync('content/post/post-1.md', 'utf8')
     expect(notDraftData).toBe(sentNotDraftData)
 
@@ -126,7 +126,7 @@ test('Saving an existing post with a changed draft status should keep the same d
         { categories: ['Cat 1', 'Cat 2'], description: 'My description' },
         'Test content'
     ).replace('---', '---\ndraft: false')
-    await Post.save(sentDraftData)
+    Post.save(sentDraftData)
     const data = fs.readFileSync('content/post/random-6.md', 'utf8')
     expect(data).toBe(sentDraftData.replace('---', '---\ndraft: true'))
 })
