5d4ce72aa2e58505f44cf06d20e06deb72127599
Marc Alexandre <github@malexandre.fr>
2017-10-02 19:04:26 +0200

    Manage the draft status in post save

--- src/post/save.js
+++ src/post/save.js
@@ -3,12 +3,38 @@ const frontMatter = require('front-matter')
 const slugify = require('slugify')
 const winston = require('winston')
 
+const setDraftStatus = (post, isAlreadyDraft = false, isOldVersionDraft = true) => {
+    if (!isAlreadyDraft && isOldVersionDraft) {
+        winston.info('Adding draft status')
+        post = post.replace(/^---\n/, '---\ndraft: true\n')
+    }
+    else if (isAlreadyDraft && !isOldVersionDraft) {
+        winston.info('Removing draft status')
+        post = post.replace(/^(---[\S\s]*?)draft: true\s([\S\s]*?---)/, '$1$2')
+    }
+
+    return post
+}
+
 const save = async(post) => {
     const data = frontMatter(post)
     const filename = `${slugify(data.attributes.title.toLowerCase())}.md`
+    const path = `content/post/${filename}`
 
-    winston.info(`Post.save: Saving post ${filename}:`, data)
-    fs.writeFileSync(`content/post/${filename}`, post)
+    let oldFile
+    try {
+        oldFile = fs.readFileSync(path, 'utf8')
+    }
+    catch (e) {
+        if (!e.message.includes('ENOENT')) {
+            winston.error('Post.save: Error while reading old file', e)
+            return
+        }
+    }
+    const oldData = oldFile ? frontMatter(oldFile) : undefined
+
+    winston.info(`Post.save: Saving post ${filename} (${oldData ? 'edit' : 'new'}):`, data)
+    fs.writeFileSync(path, setDraftStatus(post, data.attributes.draft, oldData ? !!oldData.attributes.draft : true))
 
     const postJson = Object.assign({ content: data.body }, data.attributes)
     if (postJson.categories) {
