8a1207ce2bb75f7a71ac1d996feabf324c449d7c
Marc Alexandre <github@malexandre.fr>
2017-10-03 12:31:01 +0200

    Refactor API definition with better ordering

--- src/rest/index.js
+++ src/rest/index.js
@@ -4,9 +4,75 @@ const { get, list, save, setPublish } = require('../post')
 
 const fakeHandler = (req, res) => res.sendStatus(200)
 
-const buildApi = (app) => {
-    app.get('/api/get', (req, res) => {
-        const path = req.query.path
+const apiGet = (req, res) => {
+    const path = req.query.path
+
+    if (!path) {
+        res.status(400).send('Path is mandatory')
+        return
+    }
+
+    if (typeof path !== 'string') {
+        res.status(400).send('Path should be a string')
+        return
+    }
+
+    res.status(200).json(get(path))
+}
+
+const apiList = async(req, res) => {
+    const count = parseInt(req.query.count)
+    const offset = parseInt(req.query.offset)
+
+    if (typeof req.query.filter !== 'string' && typeof req.query.filter !== 'undefined') {
+        res.status(400).send('Filter should be unique')
+        return
+    }
+
+    if ((isNaN(offset) && typeof req.query.offset !== 'undefined') || Array.isArray(req.query.offset)) {
+        res.status(400).send('Offset should be unique and an integer')
+        return
+    }
+
+    if ((isNaN(count) && typeof req.query.count !== 'undefined') || Array.isArray(req.query.count)) {
+        res.status(400).send('Count should be unique and an integer')
+        return
+    }
+
+    const results = list({
+        filter: req.query.filter,
+        orderby: req.query.orderby,
+        offset: !isNaN(offset) && offset >= 0 ? offset : 0,
+        count: !isNaN(count) && count > 0 ? count : 10
+    })
+
+    res.status(200).json(results)
+}
+
+const apiSave = (req, res) => {
+    const { post, oldPath } = req.body
+
+    if (!post) {
+        res.status(400).send('Post is mandatory')
+        return
+    }
+
+    if (typeof post !== 'string') {
+        res.status(400).send('Post should be a string')
+        return
+    }
+
+    if (typeof oldPath !== 'string' && typeof oldPath !== 'undefined') {
+        res.status(400).send('oldPath should be a string')
+        return
+    }
+
+    res.status(200).json(save(post, oldPath))
+}
+
+const apiSetPublish = (publishStatus) => {
+    return (req, res) => {
+        const { path } = req.body
 
         if (!path) {
             res.status(400).send('Path is mandatory')
@@ -18,96 +84,21 @@ const buildApi = (app) => {
             return
         }
 
-        res.status(200).json(get(path))
-    })
+        res.status(200).json(setPublish(path, publishStatus))
+    }
+}
 
-    app.get('/api/list', async(req, res) => {
-        const count = parseInt(req.query.count)
-        const offset = parseInt(req.query.offset)
+const buildApi = (app) => {
+    app.get('/api/get', apiGet)
+    app.get('/api/list', apiList)
+    app.post('/api/save', apiSave)
+    app.delete('/api/delete', fakeHandler)
 
-        if (typeof req.query.filter !== 'string' && typeof req.query.filter !== 'undefined') {
-            res.status(400).send('Filter should be unique')
-            return
-        }
-
-        if ((isNaN(offset) && typeof req.query.offset !== 'undefined') || Array.isArray(req.query.offset)) {
-            res.status(400).send('Offset should be unique and an integer')
-            return
-        }
-
-        if ((isNaN(count) && typeof req.query.count !== 'undefined') || Array.isArray(req.query.count)) {
-            res.status(400).send('Count should be unique and an integer')
-            return
-        }
-
-        const results = list({
-            filter: req.query.filter,
-            orderby: req.query.orderby,
-            offset: !isNaN(offset) && offset >= 0 ? offset : 0,
-            count: !isNaN(count) && count > 0 ? count : 10
-        })
-
-        res.status(200).json(results)
-    })
+    app.post('/api/publish', apiSetPublish(true))
+    app.post('/api/unpublish', apiSetPublish(false))
 
     app.post('/api/build', fakeHandler)
-
-    app.post('/api/publish', (req, res) => {
-        const { path } = req.body
-
-        if (!path) {
-            res.status(400).send('Path is mandatory')
-            return
-        }
-
-        if (typeof path !== 'string') {
-            res.status(400).send('Path should be a string')
-            return
-        }
-
-        res.status(200).json(setPublish(path, true))
-    })
-
-    app.post('/api/save', (req, res) => {
-        const { post, oldPath } = req.body
-
-        if (!post) {
-            res.status(400).send('Post is mandatory')
-            return
-        }
-
-        if (typeof post !== 'string') {
-            res.status(400).send('Post should be a string')
-            return
-        }
-
-        if (typeof oldPath !== 'string' && typeof oldPath !== 'undefined') {
-            res.status(400).send('oldPath should be a string')
-            return
-        }
-
-        res.status(200).json(save(post, oldPath))
-    })
-
-    app.post('/api/unpublish', (req, res) => {
-        const { path } = req.body
-
-        if (!path) {
-            res.status(400).send('Path is mandatory')
-            return
-        }
-
-        if (typeof path !== 'string') {
-            res.status(400).send('Path should be a string')
-            return
-        }
-
-        res.status(200).json(setPublish(path, false))
-    })
-
     app.post('/api/upload', fakeHandler)
-
-    app.delete('/api/delete', fakeHandler)
 }
 
 module.exports = { buildApi }
