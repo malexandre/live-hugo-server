bba0ca06e190faa5d85765c6dd26a7e934fc883d
Marc Alexandre <github@malexandre.fr>
2017-10-04 17:32:58 +0200

    Check env for config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 'use strict'
 
 module.exports = {
-    port: '3000',
-    buildCmd: 'docker-compose run build',
-    gitFolder: '../blog',
-    postFolder: '../blog/content/post',
-    uploadFolder: '../blog/assets/img'
+    port: process.env.LIVE_HUGO_PORT || 3000,
+    buildCmd: process.env.LIVE_HUGO_BUILD_CMD || 'docker-compose run build',
+    gitFolder: process.env.LIVE_HUGO_GIT_FOLDER || '../blog',
+    postFolder: process.env.LIVE_HUGO_POST_FOLDER || '../blog/content/post',
+    uploadFolder: process.env.LIVE_HUGO_UPLOAD_FOLDER || '../blog/assets/img'
 }
