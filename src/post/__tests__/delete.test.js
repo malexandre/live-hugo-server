8c66050f2e171c042fed038f05af83633c71dbe9
Marc Alexandre <github@malexandre.fr>
2017-10-04 14:14:48 +0200

    Add git sync when deleting a post

--- src/post/__tests__/delete.test.js
+++ src/post/__tests__/delete.test.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const mockFs = require('mock-fs')
 const Post = require('../')
+const Git = require('../../git')
 const { initMockFs } = require('./setup-common')
 
 jest.mock('../../config', () => ({
@@ -8,8 +9,15 @@ jest.mock('../../config', () => ({
     uploadFolder: 'assets/img'
 }))
 
+jest.mock('../../git', () => ({
+    syncFiles: jest.fn()
+}))
+
 beforeEach(() => initMockFs())
-afterEach(() => mockFs.restore())
+afterEach(() => {
+    mockFs.restore()
+    Git.syncFiles.mockClear()
+})
 
 test('Deleting an existing post should delete it in the file system', async() => {
     const path = 'content/post/post-1.md'
@@ -17,12 +25,19 @@ test('Deleting an existing post should delete it in the file system', async() =>
     expect(() => fs.readFileSync(path, 'utf8')).toThrowError('ENOENT')
 })
 
+test('Deleting an existing post should call Git.syncFiles with the right commit message', async() => {
+    const path = 'content/post/post-1.md'
+    await Post.del(path)
+    expect(Git.syncFiles).toBeCalledWith('[HugoLive] Delete post: post-1.md')
+})
+
 test('Deleting an non-existing post should do nothing', async() => {
-    expect.assertions(1)
+    expect.assertions(2)
     try {
         await Post.del('fake path')
     }
     catch (e) {
         expect(e.code).toBe('ENOENT')
     }
+    expect(Git.syncFiles).not.toBeCalledWith()
 })
--- src/post/delete.js
+++ src/post/delete.js
@@ -1,9 +1,12 @@
 const fs = require('../promise-fs/')
 const winston = require('winston')
 
+const { syncFiles } = require('../git')
+
 const del = async(path) => {
     try {
         await fs.unlinkAsync(path)
+        await syncFiles(`[HugoLive] Delete post: ${path.split('/').pop()}`)
     }
     catch (e) {
         winston.error('Post.delete: Error while deleting file', path, e)
