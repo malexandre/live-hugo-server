fcf3b26fae4c4c4517687b6f1e5897fb62d0f7eb
Marc Alexandre <github@malexandre.fr>
2017-10-04 14:36:08 +0200

    Refactor promise-fs

--- src/promise-fs/index.js
+++ src/promise-fs/index.js
@@ -1,11 +1,12 @@
 const fs = require('fs')
 const { promisify } = require('util')
 
-module.exports = Object.assign({}, fs, {
-    readdirAsync: promisify(fs.readdir),
-    readFileAsync: promisify(fs.readFile),
-    renameAsync: promisify(fs.rename),
-    statAsync: promisify(fs.stat),
-    unlinkAsync: promisify(fs.unlink),
-    writeFileAsync: promisify(fs.writeFile)
-})
+const methodList = ['readdir', 'readFile', 'rename', 'rmdir', 'stat', 'unlink', 'writeFile']
+
+const buildAsyncMethods = () => {
+    const asyncMethods = {}
+    methodList.forEach((method) => (asyncMethods[`${method}Async`] = promisify(fs[method])))
+    return asyncMethods
+}
+
+module.exports = Object.assign({}, fs, buildAsyncMethods())
