344ab5ea4c93721c0d033ddaa1d98710d8af7aa5
Marc Alexandre <github@malexandre.fr>
2017-10-07 13:50:41 +0200

    Add new config options & packages for auth

--- src/config.js
+++ src/config.js
@@ -8,4 +8,13 @@ module.exports = {
         post: process.env.LIVE_HUGO_POST_FOLDER || '../blog/content/post',
         upload: process.env.LIVE_HUGO_UPLOAD_FOLDER || '../blog/assets/img'
     },
+    tokenExpiration: {
+        access: process.env.LIVE_HUGO_ACCESS_TOKEN_EXPIRATION || 15 * 60, // In seconds
+        refresh: process.env.LIVE_HUGO_REFRESH_TOKEN_EXPIRATION || 7 * 24 * 60 * 60 // In seconds
+    },
+    redis: {
+        host: process.env.LIVE_HUGO_REDIS_HOST || 'redis',
+        port: process.env.LIVE_HUGO_REDIS_PORT || 6379
+    },
+    jwtSecret: process.env.LIVE_HUGO_JWT_SECRET || 'jyNwL1Dhm1VC0sX6cdVFELkHCXCu9lL6AylFDaWGJevTjuUlYaeDPGR3E8'
 }
